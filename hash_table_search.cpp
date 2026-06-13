933efddce2752bcd21280bcdf7a54682eb5f5501
sapuplanta <jevaanraj17@gmail.com>
2026-06-13 22:47:17 +0800

    Implement hash table search methods

--- hash_table_search.cpp
+++ hash_table_search.cpp
@@ -58,7 +58,82 @@
             }
         }
 
+        int hashFunction(long long key) const{
+            return (int)((unsigned long long)key % (unsigned long long)tableSize);
+        }
+        void insert(const Record& rec) {
+        int   idx     = hashFunction(rec.key);
+        Node* newNode = new Node();
+        newNode->data = rec;
+        newNode->next = table[idx];
+        table[idx]    = newNode;
+        numElements++;
+        }
 
+        Node* search(long long targetKey) const {
+            int idx = hashFunction(targetKey);
+            Node* curr = table[idx];
+            while (curr != nullptr) {
+                if (curr->data.key == targetKey) {
+                    return curr; 
+                }
+                curr = curr->next;
+            }
+            return nullptr; 
+        }
 
-        
+        long long getBestCaseKey() cosnt {
+            for (int i = 0; i < tableSize; ++i) {
+                if (table[i] != nullptr) {
+                    return table[i]->data.key; 
+                }
+            }
+            return LLONG_MIN;      
+        }
+
+        long long getLongestChainKey() const{
+            int maxLen=0;
+            long long worstKey = LLONG_MIN;
+
+            for (int i = 0; i < tableSize; i++){
+                if (table[i]) == nullptr) continue;
+
+                int len = 0;
+                Node* curr = table[i];
+                Node* last = nullptr;
+
+                while (curr != nullptr) {
+                    len++;
+                    last = curr;
+                    curr = curr->next;
+                }
+
+                if (len > maxLen) {
+                    maxLen = len;
+                    worstKey = last->data.key; 
+                }
+
+            }
+            return worstKey;
+        }
+
+        vector<long long> getAllKeys() const {
+            vector<long long> keys;
+            for (int i = 0; i < tableSize; ++i) {
+                Node* curr = table[i];
+                while (curr != nullptr) {
+                    keys.push_back(curr->data.key);
+                    curr = curr->next;
+                }
+            }
+            return keys;
+        }
+
+        int getNumElements() const {
+            return numElements;
+        int getTableSize() const {
+            return tableSize;
+        };
+
+         
    }
